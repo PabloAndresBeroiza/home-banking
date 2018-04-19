12bfa50cc352c60edc40e17868bcf1dfd6a98d7f
VAIO\pablo <beroiza.pablo@gmail.com>
2018-04-19 18:30:56 -0300

    extraer dinero, cambiar limite, depositar dinero

--- js/homebanking.js
+++ js/homebanking.js
@@ -10,15 +10,39 @@ actualizarLimiteEnPantalla();
 
 //Funciones que tenes que completar
 function cambiarLimiteDeExtraccion() {
-
+var nuevoLimite = parseInt(prompt("ingrese el nuevo Limite"));
+limiteExtraccion = nuevoLimite;
+actualizarLimiteEnPantalla();
+alert("Su nuevo limite de extraccion es: " + limiteExtraccion);
 }
 
 function extraerDinero() {
+var extraccion= parseInt(prompt('ingrese la cantidad de dinero'));
+var saldoAnterior = saldoCuenta;
+
+if(extraccion % 100){
+    alert("Solo puede dar billetes de 100");
+}
+if(extraccion > saldoCuenta){
+    alert('No puede sacar mas de lo que tienes');
+}
+else if (extraccion>limiteExtraccion)  {
+    alert('Elija un monto menor');
+} 
+else{
+    saldoCuenta -= extraccion;
+    alert("has sacado: $"+ extraccion +"\n Saldo anterior: "+ saldoAnterior + "\n Saldo actual: " + saldoCuenta);
+    actualizarSaldoEnPantalla();
+}
 
 }
 
 function depositarDinero() {
-
+var deposito = parseInt(prompt('ingrese la cantidad de dinero'));
+var saldoAnterior = saldoCuenta;
+saldoCuenta += deposito;
+alert("has depositado: $"+ deposito +"\n Saldo anterior: "+ saldoAnterior + "\n Saldo actual: " + saldoCuenta);
+actualizarSaldoEnPantalla();
 }
 
 function pagarServicio() {
